bd826c996e36988fd85dd60d08ab57f9f190e791
PakoSt <kokotekilata@gmail.com>
2017-12-13 09:35:57 +0000

    Wrap the text widget within a row and add a send button to trigger the action

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,12 @@
 import 'package:flutter/material.dart';
 
-
-void main(){
+void main() {
   runApp(new CracklingsparkApp());
 }
 
 class CracklingsparkApp extends StatelessWidget {
   @override
-  Widget build(BuildContext context){
+  Widget build(BuildContext context) {
     return new MaterialApp(
       title: 'Crackling Spark',
       home: new ChatScreen(),
@@ -15,7 +14,7 @@ class CracklingsparkApp extends StatelessWidget {
   }
 }
 
-class ChatScreen extends StatefulWidget{
+class ChatScreen extends StatefulWidget {
   @override
   State createState() => new ChatScreenState();
 }
@@ -24,30 +23,40 @@ class ChatScreenState extends State<ChatScreen> {
   // Add a TextEditingController to manage the interaction with the text field
   final TextEditingController _textController = new TextEditingController();
 
-  Widget _buildTextComposer(){
+  Widget _buildTextComposer() {
     return new Container(
       margin: const EdgeInsets.symmetric(horizontal: 8.0),
-      child: new TextField(
-        controller: _textController,
-        onSubmitted: _handleSubmitted,
-        decoration: new InputDecoration.collapsed(
-          hintText: "Send text message"
-        ),
+      child: new Row(
+        children: <Widget>[
+          new Flexible(
+            child: new TextField(
+              controller: _textController,
+              onSubmitted: _handleSubmitted,
+              decoration:
+                  new InputDecoration.collapsed(hintText: "Send text message"),
+            ),
+          ),
+          new Container(
+            margin: new EdgeInsets.symmetric(horizontal: 4.0),
+            child: new IconButton(
+              icon: new Icon(Icons.send),
+              onPressed: () => _handleSubmitted(_textController.text),
+            ),
+          )
+        ]
       ),
     );
   }
 
   @override
-  Widget build(BuildContext context){
+  Widget build(BuildContext context) {
     return new Scaffold(
-      appBar: new AppBar(
-        title:new Text('Crackling Spark')
-        ),
-        body: _buildTextComposer(),
+      appBar: new AppBar(title: new Text('Crackling Spark')),
+      body: _buildTextComposer(),
     );
   }
 
   void _handleSubmitted(String value) {
-      _textController.clear();
+    _textController.clear();
   }
 }
