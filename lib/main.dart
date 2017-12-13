2d200734378a3064f0e8db6cc7d1f6526d4ea404
PakoSt <kokotekilata@gmail.com>
2017-12-13 10:04:41 +0000

    - Create a list for the messages. - Modify the action to also insert the message to the list - Modify the Chatscreen to display the message list

--- lib/main.dart
+++ lib/main.dart
@@ -23,6 +23,7 @@ class ChatScreen extends StatefulWidget {
 }
 
 class ChatScreenState extends State<ChatScreen> {
+  final List<ChatMessage> _messages = <ChatMessage>[];
   final TextEditingController _textController = new TextEditingController();
 
   Widget _buildTextComposer() {
@@ -56,12 +57,35 @@ class ChatScreenState extends State<ChatScreen> {
   Widget build(BuildContext context) {
     return new Scaffold(
       appBar: new AppBar(title: new Text('Crackling Spark')),
-      body: _buildTextComposer(),
-    );
+      body: new Column(
+        children: <Widget>[
+          new Flexible(
+            child: new ListView.builder(
+              padding: new EdgeInsets.all(8.0),
+              reverse: true,
+              itemBuilder: (_, int index) => _messages[index],
+              itemCount: _messages.length,
+              )
+            ),
+            new Divider(height: 1.0,),
+            new Container(
+              decoration: new BoxDecoration(
+                color: Theme.of(context).cardColor
+              ),
+              child: _buildTextComposer(),
+            ) 
+        ])
+      );
   }
 
-  void _handleSubmitted(String value) {
+  void _handleSubmitted(String text) {
     _textController.clear();
+    ChatMessage message = new ChatMessage(
+      text: text,
+    );
+    setState((){
+      _messages.insert(0, message);
+    });
   }
 }
 
