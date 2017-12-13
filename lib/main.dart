fda5adcbae926171f7d9d276f3155ba14329459b
PakoSt <kokotekilata@gmail.com>
2017-12-13 09:17:09 +0000

    Separating the basic app to lay the groundwork for the interactive components.
    - Root-level widget for the app
    - Chat screen widget where the messages will be placed
    
    Temp:
    - Disabling the test to remove an error

--- lib/main.dart
+++ lib/main.dart
@@ -2,14 +2,26 @@ import 'package:flutter/material.dart';
 
 
 void main(){
-  runApp(
-      new MaterialApp(
-        title: 'Crackling Spark',
-        home: new Scaffold(
-          appBar: new AppBar(
-            title: new Text('Crackling Spark'),
-          ),
+  runApp(new CracklingsparkApp());
+}
+
+class CracklingsparkApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context){
+    return new MaterialApp(
+      title: 'Crackling Spark',
+      home: new ChatScreen(),
+    );
+  }
+}
+
+class ChatScreen extends StatelessWidget{
+  @override
+  Widget build(BuildContext context){
+    return new Scaffold(
+      appBar: new AppBar(
+        title:new Text('Crackling Spark')
         ),
-      )
-  );
+    );
+  }
 }
