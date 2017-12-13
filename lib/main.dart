134255db09253270caf370b3c891ccc42e48e6a6
PakoSt <kokotekilata@gmail.com>
2017-12-13 16:49:59 +0000

    Box decoration for iOS

--- lib/main.dart
+++ lib/main.dart
@@ -86,19 +86,26 @@ class ChatScreenState extends State<ChatScreen> with TickerProviderStateMixin {
           elevation:
               Theme.of(context).platform == TargetPlatform.iOS ? 0.0 : 4.0,
         ),
-        body: new Column(children: <Widget>[
-          new Flexible(
-              child: new ListView.builder(
-            padding: new EdgeInsets.all(8.0),
-            reverse: true,
-            itemBuilder: (_, int index) => _messages[index],
-            itemCount: _messages.length,
-          )),
-          new Container(
-            decoration: new BoxDecoration(color: Theme.of(context).cardColor),
-            child: _buildTextComposer(),
-          )
-        ]));
+        body: new Container(
+          child: new Column(children: <Widget>[
+            new Flexible(
+                child: new ListView.builder(
+              padding: new EdgeInsets.all(8.0),
+              reverse: true,
+              itemBuilder: (_, int index) => _messages[index],
+              itemCount: _messages.length,
+            )),
+            new Container(
+              decoration: new BoxDecoration(color: Theme.of(context).cardColor),
+              child: _buildTextComposer(),
+            )
+          ]),
+          decoration: Theme.of(context).platform == TargetPlatform.iOS
+              ? new BoxDecoration(
+                  border:
+                      new Border(top: new BorderSide(color: Colors.grey[200])))
+              : null,
+        ));
   }
 
   void _handleSubmitted(String text) {
