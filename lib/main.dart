aa3bfe8fafbd694cb662945d8b961bd331648e96
PakoSt <kokotekilata@gmail.com>
2017-12-13 16:08:43 +0000

    Create and apply customisation for iOS and Android

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/foundation.dart';
 
 const String _name = "Your Name";
+
+// Defining the theme data.
+final ThemeData kIOSTheme = new ThemeData(
+  primarySwatch: Colors.orange,
+  primaryColor: Colors.grey[100],
+  primaryColorBrightness: Brightness.light
+);
+
+final ThemeData kDefaultTheme = new ThemeData(
+  primarySwatch: Colors.purple,
+  accentColor: Colors.orangeAccent[400]
+);
+
 
 void main() {
   runApp(new CracklingsparkApp());
@@ -11,6 +25,8 @@ class CracklingsparkApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return new MaterialApp(
       title: 'Crackling Spark',
+      theme: defaultTargetPlatform == TargetPlatform.iOS ?
+      kIOSTheme : kDefaultTheme,
       home: new ChatScreen(),
     );
   }
